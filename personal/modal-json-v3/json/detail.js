55ad58589e708ead35696a40f4f474671d1294a2
Gabriel Bassino <gabriel@neotix.com.br>
2017-09-05 18:46:47 -0300

    Modal com JSON
    Agora no Github

---
+++ personal/modal-json-v3/json/detail.js
@@ -0,0 +1,519 @@
+window.detail = {
+	577: {
+		leadname: "Cursed Ice Jutsu",
+		lead: "Courage Characters ATK and SPD +50%",
+
+		skill1name: "Secret Jutsu: Thousand Needles of Death",
+		skill1ogname: "秘術・千殺水翔",
+		skill1type: "Unique",
+		skill1speed: "Fastest",
+		skill1cost: "50",
+		skill1: "Deal 20x ATK damage ignoring 40% DEF. If own SPD is buffed, deal 4x damage",
+
+
+		skill2name: "This here..... The Cursed Power",
+		skill2ogname: "これは…呪われた力",
+		skill2type: "Unique",
+		skill2speed: "Fast",
+		skill2cost: "70",
+		skill2: "Own SPD and Skill Damage +100%, and grant a high chance to Dodge Skills",
+
+		ability1name: "Snow Clan",
+		ability1: "	Skill Damage +100%, Immune to Negative Effects",
+
+		ability2name: "Murderous Heart",
+		ability2: "High Chance to have an extra turn. (You can't get an extra turn during your extra turn"
+	},
+
+	"577_6": {
+		leadname: "Modest Fortune",
+		lead: "Lightning, Storm and Taijutsu Skill Damage +40% to all charaters",
+
+		skill1name: "Storm Style: Laser Circus",
+		skill1ogname: "嵐遁・励挫鎖苛素",
+		skill1type: "Storm",
+		skill1speed: "Fastest",
+		skill1cost: "80",
+		skill1: "Deal 3% + 4,000 to al enemies dealing more damage the less enemies. Minimum 3x against 5 enemies, Maximum 20x against 1. Also has a high chance to cancel action",
+
+
+		skill2name: "Peeling off the Plating",
+		skill2ogname: "メッキ剥がしてやるよ",
+		skill2type: "Unique",
+		skill2speed: "Fast",
+		skill2cost: "50",
+		skill2: "Own ATK and Skill Damage +120% and grant \"Concentration\" state. [Concentration: Chakra Cost is reduced every turn, Max 50%]",
+
+		ability1name: "Pride of the Right Arm",
+		ability1: "ATK +100%, Incoming Lighting, Water, Unique and Storm Skill Damage are halved",
+
+		ability2name: "True Words",
+		ability2: "HP and Skill Damage +100%"
+	},
+
+	578: {
+		leadname: "Modest Fortune",
+		lead: "Lightning, Storm and Taijutsu Skill Damage +40% to all charaters",
+
+		skill1name: "Storm Style: Laser Circus",
+		skill1ogname: "嵐遁・励挫鎖苛素",
+		skill1type: "Storm",
+		skill1speed: "Fastest",
+		skill1cost: "90",
+		skill1: "Deal 3% + 2,000 to al enemies dealing more damage the less enemies. Minimum 3x against 5 enemies, Maximum 15x against 1. Also has a chance to cancel action",
+
+
+		skill2name: "Peeling off the Plating",
+		skill2ogname: "メッキ剥がしてやるよ",
+		skill2type: "Unique",
+		skill2speed: "Normal",
+		skill2cost: "70",
+		skill2: "Own ATK and Skill Damage +100% and grant \"Concentration\" state. [Concentration: Chakra Cost is reduced every turn, Max 30%]",
+
+		ability1name: "Pride of the Right Arm",
+		ability1: "ATK +70%, Reduce incoming Lighting, Water, Unique and Storm Skill Damage",
+
+		ability2name: "True Words",
+		ability2: "HP and Skill Damage +80%"
+	},
+
+	"578_6": {
+		leadname: "Modest Fortune",
+		lead: "Lightning, Storm and Taijutsu Skill Damage +40% to all charaters",
+
+		skill1name: "Storm Style: Laser Circus",
+		skill1ogname: "嵐遁・励挫鎖苛素",
+		skill1type: "Storm",
+		skill1speed: "Fastest",
+		skill1cost: "80",
+		skill1: "Deal 3% + 4,000 to al enemies dealing more damage the less enemies. Minimum 3x against 5 enemies, Maximum 20x against 1. Also has a high chance to cancel action",
+
+
+		skill2name: "Peeling off the Plating",
+		skill2ogname: "メッキ剥がしてやるよ",
+		skill2type: "Unique",
+		skill2speed: "Fast",
+		skill2cost: "50",
+		skill2: "Own ATK and Skill Damage +120% and grant \"Concentration\" state. [Concentration: Chakra Cost is reduced every turn, Max 50%]",
+
+		ability1name: "Pride of the Right Arm",
+		ability1: "ATK +100%, Incoming Lighting, Water, Unique and Storm Skill Damage are halved",
+
+		ability2name: "True Words",
+		ability2: "HP and Skill Damage +100%"
+	},
+
+	579: {
+		leadname: "End of the World",
+		lead: "Body Type and None Character Maximum HP and Skill Damage +50%",
+
+		skill1name: "Tailed Beast Bomb",
+		skill1ogname: "尾獣玉",
+		skill1type: "Tailed Beast",
+		skill1speed: "Fast",
+		skill1cost: "90",
+		skill1: "Deal 3% + 20,000 to all enemies with a chance to place the enemies under \"Misery\" state. Deal 2x damage to enemies under \"Misery\" status. [Misery: Skill Damage -20% and ATK -10%. This Status can be stacked upto 3 times.]",
+
+
+		skill2name: "Is Your Heart Broken?",
+		skill2ogname: "心を折るか…",
+		skill2type: "Unique",
+		skill2speed: "Fast",
+		skill2cost: "80",
+		skill2: "Own ATK and Skill Damage +80%, None Character's Chakra Cost -20%",
+
+		ability1name: "Joy of Battle",
+		ability1: "Maximum Chakra +50, Cast Speed +1 for 9 turns",
+
+		ability2name: "Someone Who Knows the Forbidden Jutsu",
+		ability2: "ATK and Skill Damage +50% when HP is above 50%"
+	},
+	"579_6": {
+		leadname: "End of the World",
+		lead: "Body Type and None Character Maximum HP and Skill Damage +50%",
+
+		skill1name: "Tailed Beast Bomb",
+		skill1ogname: "尾獣玉",
+		skill1type: "Tailed Beast",
+		skill1speed: "Fast",
+		skill1cost: "80",
+		skill1: "Deal 3% + 50,000 to all enemies with a chance to place the enemies under \"Misery\" state. Deal 3x damage to enemies under \"Misery\" status. [Misery: Skill Damage -20% and ATK -20%. This Status can be stacked upto 3 times.]",
+
+
+		skill2name: "Is Your Heart Broken?",
+		skill2ogname: "心を折るか…",
+		skill2type: "Unique",
+		skill2speed: "Fastest",
+		skill2cost: "70",
+		skill2: "Own ATK and Skill Damage +100%, None Character's Chakra Cost -50%",
+
+		ability1name: "Joy of Battle",
+		ability1: "Maximum Chakra +100, Cast Speed +1",
+
+		ability2name: "Someone Who Knows the Forbidden Jutsu",
+		ability2: "ATK and Skill Damage +100%"
+	},
+
+	580: {
+		leadname: "Mark of Despair",
+		lead: "Body Type and None Character ATK and SPD +50%",
+
+		skill1name: "Tailed Beast Bomb",
+		skill1ogname: "尾獣玉",
+		skill1type: "Tailed Beast",
+		skill1speed: "Fast",
+		skill1cost: "90",
+		skill1: "Deal 3% + 20,000 to all enemies with a chance to place the enemies under 'Misery' state. Deal 2x damage to enemies under 'Misery' status. [Misery: Skill Damage -20% and SPD -10%. This Status can be stacked upto 3 times.]",
+
+
+		skill2name: "Let Me Tell You About Despair",
+		skill2ogname: "絶望を教えてやろう",
+		skill2type: "Unique",
+		skill2speed: "Fast",
+		skill2cost: "80",
+		skill2: "Own ATK and Skill Damage +80%, and grant a medium chance to dodge skills to None Characters",
+
+		ability1name: "Bright Deceased Heart",
+		ability1: "Maximum Chakra +50, Cast Speed +1 for 9 turns",
+
+		ability2name: "Locked Emotions",
+		ability2: "ATK and Skill Damage +50% when HP is above 50%"
+	},
+	"580_6": {
+		leadname: "End of the World",
+		lead: "Body Type and None Character Maximum HP and Skill Damage +50%",
+
+		skill1name: "Tailed Beast Bomb",
+		skill1ogname: "尾獣玉",
+		skill1type: "Tailed Beast",
+		skill1speed: "Fast",
+		skill1cost: "90",
+		skill1: "Deal 3% + 50,000 to all enemies with a chance to place the enemies under \"Misery\" state. Deal 3x damage to enemies under \"Misery\" status. [Misery: Skill Damage -20% and SPD -20%. This Status can be stacked upto 3 times.]",
+
+
+		skill2name: "Let Me Tell You About Despair",
+		skill2ogname: "絶望を教えてやろう",
+		skill2type: "Unique",
+		skill2speed: "Fast",
+		skill2cost: "80",
+		skill2: "Own ATK and Skill Damage +100%, and grant a high chance to dodge skills to None Characters",
+
+		ability1name: "Bright Deceased Heart",
+		ability1: "Maximum Chakra +100, Cast Speed +1",
+
+		ability2name: "Locked Emotions",
+		ability2: "ATK and Skill Damage +100%"
+	},
+
+	581: {
+		leadname: "Shinobi Instructor",
+		lead: "Heart or Body Characters Maximum HP and ATK +30%",
+
+		skill1name: "Ninja Battle Technique: Ninja Dog Fang",
+		skill1ogname: "忍戦術・忍犬襲牙",
+		skill1type: "Unique",
+		skill1speed: "Fast",
+		skill1cost: "70",
+		skill1: "Deal 7% + 1,000 to one enemy with a chance for Confusion and/or Charm.  Deal more damage the more HP Kakashi has, Maximum 10x",
+
+
+		skill2name: "I Will Tell You About the Ninja Battle Technique",
+		skill2ogname: "忍戦術を教えてやる",
+		skill2type: "None",
+		skill2speed: "Normal",
+		skill2cost: "90",
+		skill2: "Own ATK, DEF and SPD +100%.  Skill Damage +50% for Konoha Characters",
+
+		ability1name: "Important Words",
+		ability1: "Maximum HP +80%, Immune to Debuff",
+
+		ability2name: "Feelings Towards Allies",
+		ability2: "Chance for Cast Speed +1 after every action"
+	},
+
+	"581_6": {
+		leadname: "Shinobi Instructor",
+		lead: "Heart or Body Characters Maximum HP and ATK +30%",
+
+		skill1name: "Ninja Battle Technique: Ninja Dog Fang",
+		skill1ogname: "忍戦術・忍犬襲牙",
+		skill1type: "Unique",
+		skill1speed: "Fast",
+		skill1cost: "70",
+		skill1: "Deal 7% + 1,000 to one enemy with a chance for Confusion and/or Charm.  Deal more damage the more HP Kakashi has, Maximum 10x",
+
+
+		skill2name: "I Will Tell You About the Ninja Battle Technique",
+		skill2ogname: "忍戦術を教えてやる",
+		skill2type: "None",
+		skill2speed: "Normal",
+		skill2cost: "90",
+		skill2: "Own ATK, DEF and SPD +100%.  Skill Damage +50% for Konoha Characters",
+
+		ability1name: "Important Words",
+		ability1: "Maximum HP +80%, Immune to Debuff",
+
+		ability2name: "Feelings Towards Allies",
+		ability2: "Chance for Cast Speed +1 after every action"
+	},
+
+	582: {
+		leadname: "Present Day Hell",
+		lead: "All Type ATK, DEF and SPD +50%.  Reduce 10% HP after every action",
+
+		skill1name: "Kamui: Disappearance",
+		skill1ogname: "神威・消魂",
+		skill1type: "Sharingan",
+		skill1speed: "Fastest",
+		skill1cost: "50",
+		skill1: "Deal 7% + 6,666 to one enemy with a chance to crit dealing 10x damage.  Greatly Shift the enemies action.",
+
+
+		skill2name: "Give Up... Disappear",
+		skill2ogname: "諦めて…消えろ",
+		skill2type: "None",
+		skill2speed: "Fast",
+		skill2cost: "90",
+		skill2: "One enemies ATK -30% with a chance to cancel the enemies' SPD buff.  All allies Sharingan Damage +100% ",
+
+		ability1name: "Give Everything Up",
+		ability1: "For 3 turns, Chakra Cost -50%.  After 3 turns, Chakra Cost -30% ",
+
+		ability2name: "Fallen Soul",
+		ability2: "Chance to dodge skills.  Skill Damage +80% when HP is above 70%"
+	},
+
+	"582_6": {
+		leadname: "Present Day Hell",
+		lead: "All Type ATK, DEF and SPD +50%.  Reduce 10% HP after every action",
+
+		skill1name: "Kamui: Disappearance",
+		skill1ogname: "神威・消魂",
+		skill1type: "Sharingan",
+		skill1speed: "Fastest",
+		skill1cost: "50",
+		skill1: "Deal 7% + 6,666 to one enemy with a chance to crit dealing 10x damage.  Greatly Shift the enemies action.",
+
+
+		skill2name: "Give Up... Disappear",
+		skill2ogname: "諦めて…消えろ",
+		skill2type: "None",
+		skill2speed: "Fast",
+		skill2cost: "90",
+		skill2: "One enemies ATK -30% with a chance to cancel the enemies' SPD buff.  All allies Sharingan Damage +100% ",
+
+		ability1name: "Give Everything Up",
+		ability1: "For 3 turns, Chakra Cost -50%.  After 3 turns, Chakra Cost -30% ",
+
+		ability2name: "Fallen Soul",
+		ability2: "Chance to dodge skills.  Skill Damage +80% when HP is above 70%"
+	},
+
+	583: {
+		leadname: "Team 7's Teacher",
+		lead: "Intellect Konoha Character's ATK, DEF and SPD +50%",
+
+		skill1name: "Ninja Battle Technique: Switching Trap",
+		skill1ogname: "忍戦術・瞬転落し",
+		skill1type: "Weapon",
+		skill1speed: "Normal",
+		skill1cost: "80",
+		skill1: "Deal 7% + 3,000 to one enemy, and grant a medium chance to dodge skills. Deal 3x damage if Kakashi can dodge skills",
+
+
+		skill2name: "Treasure Your Allies",
+		skill2ogname: "仲間を大切にしろ",
+		skill2type: "Unique",
+		skill2speed: "Fast",
+		skill2cost: "90",
+		skill2: "Own ATK, DEF and SPD +40%, Cast Speed +1 for \"Naruto Uzumaki\", \"Sasuke Uchiha\", and \"Sakura Haruno\"",
+
+		ability1name: "Hidden Ability",
+		ability1: "For 5 turns, reduce all incoming Skill Damage.  After 5 turns, Skill Damage +100%",
+
+		ability2name: "Ally's Heart",
+		ability2: "ATK, DEF and SPD +20% for each Konoha allies"
+	},
+
+	"583_6": {
+		leadname: "Team 7's Teacher",
+		lead: "Intellect Konoha Character's ATK, DEF and SPD +50%",
+
+		skill1name: "Ninja Battle Technique: Switching Trap",
+		skill1ogname: "忍戦術・瞬転落し",
+		skill1type: "Weapon",
+		skill1speed: "Fast",
+		skill1cost: "70",
+		skill1: "Deal 7% + 6,000 to one enemy, and grant a medium chance to dodge skills. Deal 10x damage if Kakashi can dodge skills",
+
+
+		skill2name: "Treasure Your Allies",
+		skill2ogname: "仲間を大切にしろ",
+		skill2type: "Unique",
+		skill2speed: "Fastest",
+		skill2cost: "80",
+		skill2: "Own ATK, DEF and SPD +80%, Cast Speed +1 for \"Naruto Uzumaki\", \"Sasuke Uchiha\", and \"Sakura Haruno\"",
+
+		ability1name: "Hidden Ability",
+		ability1: "For 5 turns, reduce all incoming damage by half.  After 5 turns, Skill Damage +150%",
+
+		ability2name: "Ally's Heart",
+		ability2: "ATK, DEF and SPD +40% for each Konoha allies"
+	},
+
+	584: {
+		leadname: "Team 7's Noisy Ninja",
+		lead: "Intellect Konoha Character's Maximum HP and Skill Damage +40%",
+
+		skill1name: "Team 7: Shadow Clone",
+		skill1ogname: "七班連係・影分身",
+		skill1type: "Clone",
+		skill1speed: "Normal",
+		skill1cost: "70",
+		skill1: "Deal 7% + 3,000 to one enemy, Deal 4x damage while under \"Connected\" state",
+
+
+		skill2name: "Everyone, Lets Go!",
+		skill2ogname: "皆、行くってばよ！",
+		skill2type: "Unique",
+		skill2speed: "Fast",
+		skill2cost: "100",
+		skill2: "Own ATK +80%, grant \"Connected\" status to \"Naruto Uzumaki\", \"Sasuke Uchiha\", and \"Sakura Haruno\". [Connected: Reduce the chance of getting aflicted by a Negative Effect and increase Skill Damage based on how many Konoha Character is in the team, Maximum 150%]",
+
+		ability1name: "Punctual Grand Lead",
+		ability1: "SPD +50%, and has a chance to have an extra turn. (You can't get an extra turn during your extra turn)",
+
+		ability2name: "Renown Heart",
+		ability2: "Immune to Negative Effects, Maximum HP +80%"
+	},
+
+	"584_6": {
+		leadname: "Team 7's Noisy Ninja",
+		lead: "Intellect Konoha Character's Maximum HP and Skill Damage +40%",
+
+		skill1name: "Team 7: Shadow Clone",
+		skill1ogname: "七班連係・影分身",
+		skill1type: "Clone",
+		skill1speed: "Fast",
+		skill1cost: "65",
+		skill1: "Deal 7% + 5,000 to one enemy, Deal 4x damage while under \"Connected\" state.  Deal 2x damage if there's an ally \"Sasuke Uchiha\" or \"Sakura Haruno\". (Max 8x damage)",
+
+
+		skill2name: "Everyone, Lets Go!",
+		skill2ogname: "皆、行くってばよ！",
+		skill2type: "Unique",
+		skill2speed: "Fastest",
+		skill2cost: "80",
+		skill2: "Own ATK +100%, grant \"Connected\" status to \"Naruto Uzumaki\", \"Sasuke Uchiha\", and \"Sakura Haruno\". [Connected: Reduce the chance of getting aflicted by a Negative Effect and increase Skill Damage based on how many Konoha Character is in the team, Maximum 200%]",
+
+		ability1name: "Punctual Grand Lead",
+		ability1: "SPD +80%, and has a high chance to have an extra turn. (You can't get an extra turn during your extra turn)",
+
+		ability2name: "Renown Heart",
+		ability2: "Immune to Negative Effects, Maximum HP +120%"
+	},
+
+	585: {
+		leadname: "Team 7's Genius Ninja",
+		lead: "Intellect Konoha Character's SPD and Skill Damage +40%",
+
+		skill1name: "Team 7: Fireball Jutsu",
+		skill1ogname: "七班連係・豪火球の術",
+		skill1type: "Fire",
+		skill1speed: "Normal",
+		skill1cost: "70",
+		skill1: "Deal 7% + 3,000 to one enemy, with a chance to Burn.  Deal 4x damage to Burning enemies",
+
+
+		skill2name: "Don't Get in My Way",
+		skill2ogname: "足手まといになるなよ",
+		skill2type: "Unique",
+		skill2speed: "Fast",
+		skill2cost: "100",
+		skill2: "Own SPD +80%, grant \"Connected\" status to \"Naruto Uzumaki\", \"Sasuke Uchiha\", and \"Sakura Haruno\". [Connected: Reduce the chance of getting aflicted by a Negative Effect and increase Skill Damage based on how many Konoha Character is in the team, Maximum 150%]",
+
+		ability1name: "Carrying the Family Name",
+		ability1: "SPD and Skill Damage +80%",
+
+		ability2name: "Hatred of the Heart",
+		ability2: "ATK +30% after every action (Max 120%)"
+	},
+
+	"585_6": {
+		leadname: "Team 7's Bright Flower",
+		lead: "Intellect Konoha Character's DEF and Skill Damage +40%",
+
+		skill1name: "Team 7: Fireball Jutsu",
+		skill1ogname: "七班連係・豪火球の術",
+		skill1type: "Fire",
+		skill1speed: "Fast",
+		skill1cost: "65",
+		skill1: "Deal 7% + 5,000 to one enemy, with a chance to Burn.  Deal 4x damage to Burning enemies. Deal 2x damage if there's an ally \"Sasuke Uchiha\" or \"Sakura Haruno\". (Max 8x damage)",
+
+
+		skill2name: "Don't Get in My Way",
+		skill2ogname: "足手まといになるなよ",
+		skill2type: "Unique",
+		skill2speed: "Fastest",
+		skill2cost: "80",
+		skill2: "Own SPD +100%, grant \"Connected\" status to \"Naruto Uzumaki\", \"Sasuke Uchiha\", and \"Sakura Haruno\". [Connected: Reduce the chance of getting aflicted by a Negative Effect and increase Skill Damage based on how many Konoha Character is in the team, Maximum 200%]",
+
+		ability1name: "Carrying the Family Name",
+		ability1: "SPD and Skill Damage +80%",
+
+		ability2name: "Hatred of the Heart",
+		ability2: "ATK +30% after every action (Max 120%)"
+	},
+
+	586: {
+		leadname: "Team 7's Genius Ninja",
+		lead: "Intellect Konoha Character's SPD and Skill Damage +40%",
+
+		skill1name: "Team 7: Shannaro!",
+		skill1ogname: "七班連係・しゃーんなろー！",
+		skill1type: "Taijutsu",
+		skill1speed: "Normal",
+		skill1cost: "70",
+		skill1: "Deal 10x ATK Damage to one enemy ignoring 10% DEF. Deal 4x damage while under \"Concentration\" state",
+
+
+		skill2name: "Diversion Tactic",
+		skill2ogname: "陽動作戦よっ！",
+		skill2type: "Unique",
+		skill2speed: "Fast",
+		skill2cost: "100",
+		skill2: "Chance to reduce one enemy's DEF by 30%, and increase their ATK and/or SPD by 30%.  Grant \"Concentration\" state to \"Naruto Uzumaki\", \"Sasuke Uchiha\", and \"Sakura Haruno\" [Concentration: Reduce Chakra Cost after every action. Max -20%] ",
+
+		ability1name: "Love Rather Than Training",
+		ability1: "Maximum HP +50%, reduce the chance of being debuffed by half",
+
+		ability2name: "One Sasuke",
+		ability2: "SPD and ATK +50%"
+	},
+
+	"586_6": {
+		leadname: "Team 7's Genius Ninja",
+		lead: "Intellect Konoha Character's SPD and Skill Damage +40%",
+
+		skill1name: "Team 7: Shannaro!",
+		skill1ogname: "七班連係・しゃーんなろー！",
+		skill1type: "Taijutsu",
+		skill1speed: "Fast",
+		skill1cost: "65",
+		skill1: "Deal 12x ATK Damage to one enemy ignoring 20% DEF. Deal 4x damage while under \"Concentration\" state. Deal 2x damage if there's an ally \"Sasuke Uchiha\" or \"Sakura Haruno\". (Max 96x damage)",
+
+
+		skill2name: "Diversion Tactic",
+		skill2ogname: "陽動作戦よっ！",
+		skill2type: "Unique",
+		skill2speed: "Fastest",
+		skill2cost: "80",
+		skill2: "Chance to reduce one enemy's DEF by 50%, and increase their ATK and/or SPD by 20%.  Grant \"Concentration\" state to \"Naruto Uzumaki\", \"Sasuke Uchiha\", and \"Sakura Haruno\" [Concentration: Reduce Chakra Cost after every action. Max -30%] ",
+
+		ability1name: "Love Rather Than Training",
+		ability1: "Maximum HP +120%, Immune to debuff",
+
+		ability2name: "One Sasuke",
+		ability2: "SPD and ATK +80%"
+	},
+}
