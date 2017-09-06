4ac129dac2790108a46e56b3fbe5c2b3ae5411ee
Gabriel Bassino <gabriel@neotix.com.br>
2017-09-06 18:05:36 -0300

    Visual novo
    Já preparando para a próxima atualização

--- personal/modal-json-v3/fullarts.sh
+++ personal/modal-json-v3/fullarts.sh
@@ -1,4 +1,7 @@
-files=( 20000001 20000011 20000021  )
+function _wget() { curl "${1}" -o $(basename "${1}") ; };
+alias wget='_wget'
+
+files=( 20000001 20000011 20000021 20001528  )
 count=0
 start=http://cdn.s3.gsnarutodash.narucole.jp/asset/master/gui___gui_cha_card___
 end=".png"
@@ -7,6 +10,6 @@ for i in ${files[@]} {
 do
     char=$i
     filename=($start$char$end)
-    wget $filename
+    _wget $filename
     let count++
 done
